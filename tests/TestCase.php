30b6027a4a10b993565b7365a4c8faf5fd2dcf80
ramunasd <ramunas.danupas@gmail.com>
2026-06-02 13:29:38 +0300

    fix undeclared $latestResponse error in tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,6 +7,9 @@ use Orchestra\Testbench\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
+    /** Ensures the property exists regardless of the resolved testbench-core/laravel pairing. */
+    public static $latestResponse;
+
     protected function getPackageProviders($app): array
     {
         return [
