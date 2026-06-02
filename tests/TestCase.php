3c4143e6e3a21aefb569e12d2645fb293c51dce1
ramunasd <ramunas.danupas@gmail.com>
2026-06-02 13:35:11 +0300

    fix undeclared $latestResponse error in tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,12 +2,17 @@
 
 namespace JustBetter\DynamicsClient\Tests;
 
+use Illuminate\Testing\TestResponse;
 use JustBetter\DynamicsClient\ServiceProvider;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    /** Ensures the property exists regardless of the resolved testbench-core/laravel pairing. */
+    /**
+     * Ensures the property exists regardless of the resolved testbench-core/laravel pairing.
+     *
+     * @var TestResponse|null
+     */
     public static $latestResponse;
 
     protected function getPackageProviders($app): array
